e5242398982ab6781da5f44a6a85effe9946f5fa
pu2oli <38104346+pu2oli@users.noreply.github.com>
2022-05-16 12:20:25 -0300

    30

--- app3.js
+++ app3.js
@@ -1,28 +1,29 @@
 function validaCredenciais(context, logins, username, password) {
-    var resultado = false
-    context.log(logins)
+    context.log('---- Autenticação ----');
+    var resultado = false;
     logins.forEach(login => {
-        context.log(login)
-        if(login.cpf == username && login.password == password ){
-            context.log('Entrei no IF true')
+        if(login.cpf == username && login.password == password ) {  
+            context.log('Autenticado com sucesso!');
+            context.log(login.cpf);
+            context.log(login.password);
             resultado = true;
         }
     });
-    return resultado
+    if (!resultado) {
+        context.log('Usuário ou senha invávilos');
+    }
+    context.log('----------------');
+    return resultado;
 }
 
 module.exports = async function (context, req) {
-    const {verify} = require('hcaptcha');
     const axios = require('axios');
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    //HCaptcha
-    // Versão Free const SECRET_KEY = '0x54706B771335AEB2e98E2396780c4478589652Eb'
-    // Versão Enterprise
-    const SECRET_KEY = '0x6c1a67c0e194D2AE71ADDE83C9b146B8B7FBF602'
-    const VERIFY_URL = 'https://hcaptcha.com/siteverify'
+    // hCaptcha - Versão Enterprise
+    const SECRET_KEY = '0x6c1a67c0e194D2AE71ADDE83C9b146B8B7FBF602';
+    const VERIFY_URL = 'https://hcaptcha.com/siteverify';
 
-    //const name = (req.query.name || (req.body && req.body.name));
     const logins = [
         {
             cpf: "39020175890",
@@ -40,30 +41,40 @@ module.exports = async function (context, req) {
             cpf: "09988776655",
             password: "302010"
         }
-    ]
+    ];
 
     var username = req.body.cpf;
     var password = req.body.senha;
-    var HCaptchaResponseToken = req.body['h-captcha-response']
-    var sucesso = false
+    var HCaptchaResponseToken = req.body['h-captcha-response'];
+    var GCaptchaResponseToken = req.body['g-captcha-response'];
+    var sucesso = false;
 
-    if(!HCaptchaResponseToken){
-        context.log('Sem token HCaptcha')
+    context.log('---- Inputs ----');
+    context.log('token hCaptcha: ' + HCaptchaResponseToken);
+    context.log('token gCaptcha: ' + GCaptchaResponseToken);
+    context.log('user:' + username);
+    context.log('pass: ' + password);
+    context.log('----------------');
+
+    if(!HCaptchaResponseToken && !GCaptchaResponseToken) {
+        context.log('Sem token HCaptcha');
         context.res = {
                     status: 401,
                     body: 'Usuário ou Senha inválidos'
                 };
         return false;
     }else{
-        let res = await axios.post('https://hcaptcha.com/siteverify', 
+        let res = await axios.post(VERIFY_URL, 
             new URLSearchParams({ 
                 secret: SECRET_KEY, response: HCaptchaResponseToken 
                 }));
 
-        var retornoHCaptcha = res.data.success
-        context.log('---------- retorno HCaptcha -----------------')
-        context.log(retornoHCaptcha)
-        context.log('---------------------------------------------')
+        var retornoHCaptcha = res.data.success;
+        context.log('---------- retorno hCaptcha -----------------');
+        context.log('Status: ' + retornoHCaptcha);
+        context.log('Data..: ' + res.data);
+        context.log('Score.: ' + res.score);
+        context.log('---------------------------------------------');
 
         if(!retornoHCaptcha){
             context.res = {
@@ -72,15 +83,15 @@ module.exports = async function (context, req) {
                     };
             return false;
         }else{
-            var retorno = await validaCredenciais(context, logins, username, password)
-            context.log('Var de retorno')
-            context.log(retorno)
+            var retorno = await validaCredenciais(context, logins, username, password);
+            context.log('Var de retorno');
+            context.log(retorno);
                 
                 if(retorno){
                     context.res = {
                         status: 200,
                         body: 'Login realizado com sucesso'
-                    };                    
+                    };
                 }
                 else{
                     context.res = {
